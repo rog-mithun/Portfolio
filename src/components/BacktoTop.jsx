a6c1c7d3b7fdb5dc84e30c292fc3ac10caefd26c
Mithunsankar <mithunsankarsivakumar@gmail.com>
2025-06-14 17:46:57 +0530

    Updated

--- src/components/BacktoTop.jsx
+++ src/components/BacktoTop.jsx
@@ -14,19 +14,10 @@ export default function BackToTop() {
     visible && (
       <button
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        className="fixed bottom-4 right-4 md:right-6 bg-blue-500 text-white p-3 rounded-full shadow-md z-50 hover:opacity-90 transition-transform transform hover:scale-105"
+        className="fixed bottom-4 right-4 sm:flex hidden bg-primary text-white p-3 rounded-full shadow-md z-50 hover:opacity-90 transition-transform transform hover:scale-105"
         aria-label="Back to top"
-        style={{
-          maxWidth: "48px",
-          maxHeight: "48px",
-          width: "12vw",
-          height: "12vw",
-          minWidth: "40px",
-          minHeight: "40px",
-          boxSizing: "border-box"
-        }}
       >
-        <FaArrowUp className="text-sm sm:text-base" />
+        <FaArrowUp className="text-base sm:text-lg" />
       </button>
     )
   );
