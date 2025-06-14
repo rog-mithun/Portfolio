1cf61dc0be55512e5e9bf40e8c8b733018af6c94
Mithunsankar <mithunsankarsivakumar@gmail.com>
2025-06-14 22:07:21 +0530

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full text-center py-4 text-sm font-medium bg-gradient-to-r from-purple-200 via-blue-100 to-pink-300 text-black dark:text-white mt-10">
+    <footer className="w-full text-center py-4 text-sm font-medium bg-gradient-to-b from-gray-900 to-black text-white">
       © Designed & developed by <span className="font-semibold">Mithunsankar</span>
     </footer>
   );
