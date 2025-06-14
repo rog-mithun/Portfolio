a11640323bf097e197fdc6010f62bbb1cb3a1dda
Mithunsankar <mithunsankarsivakumar@gmail.com>
2025-06-14 22:10:43 +0530

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
+import SparkleBackground from './SparkleBackground';
 export default function Footer() {
   return (
     <footer className="w-full text-center py-4 text-sm font-medium bg-gradient-to-b from-gray-900 to-black text-white">
+        {/* Animated Background */}
+                          <SparkleBackground />
       © Designed & developed by <span className="font-semibold">Mithunsankar</span>
     </footer>
   );
